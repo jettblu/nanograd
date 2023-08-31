34492112d3956abb163f0aa8c97e0dad397c19f1
jettblu <jettblu.fabrications@gmail.com>
2023-08-31 16:21:04 -0400

    Add tensor traits

---
+++ src/traits/mod.rs
@@ -0,0 +1,18 @@
+use std::ops::{ Add, Mul };
+use std::fmt::Display;
+use std::fmt::Debug;
+
+extern crate num;
+use self::num::traits::Zero;
+
+pub trait TensorTrait<T>: Zero +
+    Clone +
+    Copy +
+    Display +
+    Debug +
+    Add<T, Output = T> +
+    Mul<T, Output = T> {}
+
+impl<T> TensorTrait<T>
+    for T
+    where T: Zero + Clone + Copy + Display + Debug + Add<T, Output = T> + Mul<T, Output = T> {}
