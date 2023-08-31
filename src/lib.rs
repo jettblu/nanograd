8b7d9c435cb023f333fe659bde832cc123e4fafe
jettblu <jettblu.fabrications@gmail.com>
2023-08-31 16:22:34 -0400

    Update library exports

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,24 @@ pub use crate::layer::Layer;
 
 mod mlp;
 pub use crate::mlp::MLP;
+
+mod tensor;
+pub use crate::tensor::Tensor;
+
+mod types;
+pub use crate::types::device::Device;
+pub use crate::types::device::default_device;
+pub use crate::types::ops::LoadOps;
+pub use crate::types::ops::MovementOps;
+pub use crate::types::ops::UnaryOps;
+pub use crate::types::ops::BinaryOps;
+pub use crate::types::ops::ReduceOps;
+pub use crate::types::ops::TernaryOps;
+pub use crate::types::data::Dimensions;
+pub use crate::types::data::DType;
+pub use crate::types::data::default_data_type;
+pub use crate::types::data::LazyBuffer;
+pub use crate::types::data::DataArray;
+
+mod traits;
+pub use crate::traits::TensorTrait;
