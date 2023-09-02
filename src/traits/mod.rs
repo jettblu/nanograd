12f530941e01f6074654d983c23890101932bb67
jettblu <jettblu.fabrications@gmail.com>
2023-09-01 22:14:50 -0400

    Update trait bounds

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,21 +1,34 @@
-use std::ops::{ Add, Mul };
+use std::ops::{ Add, Mul, Sub };
 use std::fmt::Display;
 use std::fmt::Debug;
 
 extern crate num;
 use self::num::traits::Zero;
 use self::num::traits::One;
+// use -1 trait
+use self::num::traits::Signed;
 
 pub trait TensorTrait<T>: Zero +
     One +
     Clone +
     Copy +
+    TryFrom<f64> +
     Display +
     Debug +
     Add<T, Output = T> +
-    Mul<T, Output = T> {}
+    Mul<T, Output = T> +
+    Sub<T, Output = T> {}
 
 impl<T> TensorTrait<T>
     for T
     where
-        T: Zero + One + Clone + Copy + Display + Debug + Add<T, Output = T> + Mul<T, Output = T> {}
+        T: Zero +
+            One +
+            Clone +
+            Copy +
+            TryFrom<f64> +
+            Display +
+            Debug +
+            Add<T, Output = T> +
+            Mul<T, Output = T> +
+            Sub<T, Output = T> {}
