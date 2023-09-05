e0f8350b9816b578c6745966b298e562f5e2e563
jettblu <jettblu.fabrications@gmail.com>
2023-09-05 19:03:49 -0400

    Update main example

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,14 @@ struct Observation {
 fn main() {
     let data_a = vec![1.0, 2.0, 3.0, 4.0].into_boxed_slice();
     let data_b = vec![10.0, 20.0, 30.0, 40.0].into_boxed_slice();
-    let dimensions = (2, 2);
-    let tensor_a = Tensor::new(data_a, dimensions, None, None);
-    let tensor_b = Tensor::new(data_b, dimensions, None, None);
-    let tensor_c = tensor_a + tensor_b;
-    println!("{:?}", tensor_c.data());
+    let data_d = vec![5.0, 6.0, 7.0, 8.0].into_boxed_slice();
+    let dim_a = (2, 2);
+    let dim_b = (2, 2);
+    let tensor_a = Tensor::new(data_a, dim_a, None, Some(true));
+    let tensor_b = Tensor::new(data_b, dim_b, None, Some(true));
+    let tensor_c = tensor_a * tensor_b;
+    let tensor_d = Tensor::new(data_d, dim_a, None, Some(true));
+    let mut tensor_e = tensor_c + tensor_d;
+    tensor_e.backward();
+    println!("{:}", tensor_e);
 }
